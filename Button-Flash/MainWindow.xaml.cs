8938f1f8959a7105fc897ad1970f71e2dd322362
Nurik <tanatov1989@mail.ru>
2018-01-29 17:17:29 +0600

    Добавьте файлы проекта.

---
+++ Button-Flash/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Button_Flash
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int way;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void GoMove_Click(object sender, MouseEventArgs e)
+        {
+            Random rand = new Random();
+            way = rand.Next(4);
+            if (way==1)
+                TryToCatch.VerticalAlignment = VerticalAlignment.Bottom;
+            else if(way==2)
+                TryToCatch.VerticalAlignment = VerticalAlignment.Top;
+
+           else  if (way==3)
+                TryToCatch.HorizontalAlignment = HorizontalAlignment.Left;
+            else if(way==4)
+                TryToCatch.HorizontalAlignment = HorizontalAlignment.Right;
+        }
+
+    }
+}
